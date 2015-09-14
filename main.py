e1e0190704dcc3e274089f333dbe2ea78d6a0933
ekkleesia2 <nobreakmyheart@gmail.com>
2015-09-14 00:17:31 -0430

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+import cyclone.web
+import sys
 import os
-import tornado.httpserver
-import tornado.ioloop
-import tornado.web
 
-class MainHandler(tornado.web.RequestHandler):
+from twisted.internet import reactor
+from twisted.python import log
+
+
+class MainHandler(cyclone.web.RequestHandler):
     def get(self):
-        self.write('Hello Mundo')
+        self.write("Hello, world")
 
-def main():
-    application = tornado.web.Application([
-        (r"/", MainHandler),
-    ])
-    http_server = tornado.httpserver.HTTPServer(application)
-    port = int(os.environ.get("PORT", 5000))
-    http_server.listen(port)
-    tornado.ioloop.IOLoop.instance().start()
 
 if __name__ == "__main__":
-    main()
+    application = cyclone.web.Application([
+        (r"/", MainHandler)
+    ])
+
+    log.startLogging(sys.stdout)
+    reactor.listenTCP(os.environ.get("PORT", 5000), application)
+    reactor.run()
